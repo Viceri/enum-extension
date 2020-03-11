c3b6eabf10ef4864cd00edda3ab750b7138dacf2
Filipe de Oliveira Ribeiro <foribeiro@viceri.com.br>
2020-03-11 19:10:01 -0300

    first commit

---
+++ EnumExtensionTest/EnumExtensionTest.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using Xunit;
+
+namespace EnumExtension.testing
+{
+    public class EnumExtensionTest
+    {
+        [Theory]
+        [InlineData(Sex.Male, "Male M")]
+        [InlineData(Sex.Female, "Female M")]
+        [InlineData(Sex.Unisex, "Unisex M")]
+        public void Should_Return_Enum_Description(Sex sex, string expectedValue)
+        {
+            var enumValue = EnumExtension.GetDescription(sex);
+
+            Assert.Equal(enumValue, expectedValue);
+        }
+    }
+
+    public enum Sex
+    {
+        [Description("Male M")]
+        Male,
+
+        [Description("Female M")]
+        Female,
+
+        [Description("Unisex M")]
+        Unisex,
+    }
+}
